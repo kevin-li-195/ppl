fd9932dbfae66bf67fa3ea26c9513ef620ee87da
Kevin Li <github@mail.kevinl.io>
2017-05-26 16:28:21 -0400

    wip

--- src/Main.hs
+++ src/Main.hs
@@ -1,67 +1,27 @@
-{-# LANGUAGE ConstraintKinds #-}
-{-# LANGUAGE DataKinds #-}
-{-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE PolyKinds #-}
-{-# LANGUAGE TypeFamilies #-}
-{-# LANGUAGE TypeOperators #-}
-{-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE TypeOperators, DataKinds #-}
+
 module Main where
 
-import Data.Kind
-import GHC.TypeLits
+import Model.Types
+import Model
 
-type family IsElem (x :: k) (xs :: [k]) where
-  IsElem x xs = IsElem' x xs xs
+type MyModel
+  = "bias" :=: Double
+  :|: "bias" :=: Double |-> "coin" :=: Bool
+  :|: "bias" :=: Double |-> "numHeads" :=: Int
 
-class IsElem' (x :: k) (xs :: [k]) (orig :: [k])
+type DupModel
+  = "coin" :=: Double
+  :|: "undecl" :=: Double |-> "bias" :=: Bool
 
-instance
-  TypeError (
-    Text "The type: "
-    :<>:
-    ShowType x
-    :$$:
-    Text "is not in the list: "
-    :<>:
-    ShowType orig
-  ) => IsElem' x '[] orig
+rec :: Observation MyModel
+rec = undefined
 
-instance {-# OVERLAPPING #-} IsElem' x (x ': xs) orig
+model :: ValidModel MyModel => Model MyModel
+model = undefined
 
-instance {-# OVERLAPPABLE #-} IsElem' x xs orig => IsElem' x (y ': xs) orig
-
-type family Check' (decls :: [Symbol]) (code :: *) :: Constraint where
-  Check' decls (Declare s :> c) = Check' (s ': decls) c
-  Check' decls (Use s :> c) = IsElem s decls
-  Check' decls Done = ()
-  Check' decls (l :<|> r) = (Check' decls l, Check' decls r)
-  Check' decls c = TypeError (
-      Text "Could not validate model. It's probably badly formed." 
-      :$$: ShowType decls 
-      :$$: ShowType c
-      )
-
-type Check code = Check' '[] code
-
--- | Named variable.
-data (s :: Symbol) :=: (a :: *)
-infixr 7 :<:
-
--- | Directed dependency.
-data a |-> b
-infixr 6 |->
-
--- | Separates dependency definitions in graph.
-data a :|: b = a :|: b
-infixr 5 :|:
-
--- | Closed type family for a probabilistic model.
-type family Model m :: * where
-    Model (name :=: t) = t
-    Model (a |-> b) = Model a -> Model b
-    Model (a :|: b) = Model a :|: Model b
+dupModel :: ValidModel DupModel => Model DupModel
+dupModel = undefined
 
 main :: IO ()
 main = putStrLn "Hello, Haskell!"
